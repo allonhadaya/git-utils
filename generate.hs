1704bee3f1fbb0a3613acaf15ca719120be9ece4
Allon Hadaya <allon@ahadaya.com>
2013-10-30 20:37:46 +0000

    Prepare for errol awesomeness.

--- generate.hs
+++ generate.hs
@@ -5,10 +5,10 @@ import System.FilePath
 
 scripts :: [(FilePath, String, String)]
 scripts = [
-          ("pullall", "Pulling", "git fetch && git pull origin $(git rev-parse --symbolic-full-name --abbrev-ref HEAD)"),
-          ("statall", "Status", "git status" ++ bashPause),
-          ("branchall", "Branches", "git branch" ++ bashPause),
-          ("pruneall", "Pruning", "git fetch && git branch --merged | grep -v '\\*' | xargs -n 1 git branch -d")
+          ("pull", "Pulling", "git fetch && git pull origin $(git rev-parse --symbolic-full-name --abbrev-ref HEAD)"),
+          ("stat", "Status", "git status" ++ bashPause),
+          ("branch", "Branches", "git branch" ++ bashPause),
+          ("prune", "Pruning", "git fetch && git branch --merged | grep -v '\\*' | xargs -n 1 git branch -d")
           ]
 
 bashPause :: String
@@ -27,18 +27,29 @@ writeScript (name, message, command) = do
 
 script :: String -> String -> String
 script message command =  "#!/bin/bash \n\n\
-\cd ${1-'.'} \n\n\
-\for file in * \n\
-\do \n\
-\  if [ -d $file ] \n\
-\  then \n\
-\    cd $file \n\
-\    if [ -d '.git' ] \n\
-\    then \n\
+\run_command() {\n\
+\  if [ -d $1 ]\n\
+\  then\n\
+\    cd $1\n\
+\    if [ -d '.git' ]\n\
+\    then\n\
 \      M='" ++ message ++ "' \n\
 \      echo ${M-''} `pwd` `git branch 2>/dev/null | grep -e '^*' | sed -E 's/^\\* (.+)$/(\\1) /'` \n\
 \      " ++ command ++ " \n\
-\    fi \n\
-\    cd ../ \n\
-\  fi \n\
-\done"
+\    fi\n\
+\    cd - 1>/dev/null\n\
+\  fi\n\
+\}\n\n\
+\while getopts ':a' opt; do\n\
+\  case $opt in\n\
+\    a)\n\
+\      shift\n\
+\      cd ${1-'.'}\n\
+\      for entry in *; do \n\
+\        run_command $PWD'/'$entry\n\
+\      done\n\
+\      exit 0\n\
+\      ;;\n\
+\  esac\n\
+\done\n\n\
+\run_command ${1-'.'}"
