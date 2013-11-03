2b28c8a6e4feccbb8147a14f37a47a181e4f2bc8
Allon Hadaya <allon@ahadaya.com>
2013-11-03 00:06:20 +0000

    Rename scripts. Add help messages. Support -a and --all. Support indicating dir before flags.

--- generate.hs
+++ generate.hs
@@ -4,27 +4,61 @@ import System.IO
 import System.FilePath
 import Unlinesv
 
-scripts :: [(FilePath, String, String, Bool)]
+data Script = Script { help :: String
+                     , name :: String
+                     , label :: String
+                     , body :: String
+                     , pause :: Bool
+                     }
+bin :: FilePath
+bin = "bin"
+
 scripts = [
-          ("gpull", "Pulling", "git fetch && git pull origin $(git rev-parse --symbolic-full-name --abbrev-ref HEAD)", False),
-          ("gstat", "Status", "git status", True),
-          ("gbranch", "Branches", "git branch", True),
-          ("gprune", "Pruning", "git fetch && git branch --merged | grep -v '\\*' | xargs -n 1 git branch -d", False)
+          Script {
+            help  = "Fetches and then pulls HEAD from origin.",
+            name  = "pu",
+            label = "Pulling",
+            body  = "git fetch && git pull origin $(git rev-parse --symbolic-full-name --abbrev-ref HEAD)",
+            pause = False
+          },
+          Script {
+            help  = "Prints repository status.",
+            name  = "st",
+            label = "Status",
+            body  = "git status",
+            pause = True
+          },
+          Script {
+            help  = "Prints local branches.",
+            name  = "br",
+            label = "Branches",
+            body  = "git branch",
+            pause = True
+          },
+          Script {
+            help  = "Deletes the local branches that have been merged.",
+            name  = "pr",
+            label = "Pruning",
+            body  = "git fetch && git branch --merged | grep -v '\\*' | xargs -n 1 git branch -d",
+            pause = False
+          }
           ]
 
 main = do
-  createDirectoryIfMissing True "bin"
-  mapM_ writeScript scripts
+  createDirectoryIfMissing True bin
+  mapM_ write scripts
 
-writeScript :: (FilePath, String, String, Bool) -> IO ()
-writeScript (name, message, command, pause) = do
-  let path = combine "bin" name
-  writeFile path (script message command pause)
-  p <- getPermissions path
-  setPermissions path (p {executable = True})
+write s = do
+  let p = path s
+  writeFile p $ render s
+  permissions <- getPermissions p
+  setPermissions p $ permissions {executable = True}
 
-script :: String -> String -> Bool -> String
-script message command pause = unlinesv
+path :: Script -> FilePath
+path s = combine bin $ name s
+
+render :: Script -> String
+render s = unlinesv
   "#!/bin/bash"
   ""
   "run_command() {"
@@ -32,9 +66,9 @@ script message command pause = unlinesv
   "  then"
   "    if [ -d '.git' ]"
   "    then"
- ("      M='" ++ message ++ "'")
+ ("      M='" ++ label s ++ "'")
   "      echo ${M-''} `pwd` `git branch 2>/dev/null | grep -e '^*' | sed -E 's/^\\* (.+)$/(\\1) /'`"
- ("      " ++ command)
+ ("      " ++ body s)
   "      cd - 1>/dev/null"
   "    else"
   "      cd - 1>/dev/null"
@@ -45,17 +79,37 @@ script message command pause = unlinesv
   "  fi"
   "}"
   ""
-  "while getopts ':a' opt; do"
-  "  case $opt in"
-  "    a)"
-  "      shift"
-  "      cd ${1-'.'}"
-  "      for entry in *; do"
- ("        run_command $PWD'/'$entry " ++ if pause then "&& read -p '...' && echo ''" else "# no pause")
-  "      done"
-  "      exit 0"
-  "      ;;"
-  "  esac"
+  "ALL=false"
+  "DIR='.'"
+  ""
+  "for i in \"$@\""
+  "do"
+  "case $i in"
+  "  -a|--all)"
+  "    ALL=true"
+  "    ;;"
+  "  -h|--help)"
+ ("    echo '" ++ help s ++ "'")
+  "    exit 0"
+  "    ;;"
+  "  *)"
+  "    if [ -d $i ]"
+  "    then"
+  "      DIR=$i"
+  "    else"
+  "      echo \"unknown directory: $i\" 1>&2"
+  "      exit 1"
+  "    fi"
+  "    ;;"
+  "esac"
   "done"
   ""
-  "run_command ${1-'.'}" 
+  "if $ALL"
+  "then"
+  "  cd $DIR"
+  "  for ENTRY in *; do"
+ ("    run_command $PWD'/'$ENTRY " ++ if pause s then "&& read -p '...' && echo ''" else "# no pause")
+  "  done"
+  "else"
+  "  run_command $DIR"
+  "fi"
